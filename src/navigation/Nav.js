8cd1c0359d0ecec1320584411c25b17d8a744878
Suhel Patel <suhel.softcolon@gmail.com>
2024-02-12 22:25:28 +0530

    navigation bar

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,5 +1,32 @@
 import React from "react";
 import "./nav.css";
+
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
 export default function Nav() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          placeholder="Enter your search shoes"
+          className="search-input"
+        />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+        <a href="#"></a>
+      </div>
+    </nav>
+  );
 }
